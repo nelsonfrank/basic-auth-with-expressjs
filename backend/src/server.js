05ed1cd38c6bc50cea5bcfce94834d436e3319c4
nelsonfrank <nelsonfr78@outlook.com>
2021-10-04 17:46:46 +0300

    feat: Add not found error handler
    - 404 error handler

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,6 +14,11 @@ app.use(express.json());
 // Auth routes
 app.use("/auth", require("./auth/auth.routes"));
 
+// 404 Error Handler
+app.use((req, res, next) => {
+	res.status(400).send("Not found");
+});
+
 // 500 Error Handler
 app.use((err, req, res, next) => {
 	console.error(err.stack);
