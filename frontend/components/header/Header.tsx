2a1519187d056b3304c20cf711acc1d48a19bbb2
nelson frank <nelsonfr78@outlook.com>
2021-11-21 23:03:55 +0300

    feat: Change header menu on login and after login

--- frontend/components/header/Header.tsx
+++ frontend/components/header/Header.tsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Image from "next/image";
 import logo from "@/public/icon/logo.svg";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Header = () => {
+	const [token, setToken] = useState<string>()
+	const router = useRouter();
+
+	useEffect(() => {
+		const token = localStorage.getItem("auth_token");
+		setToken(token);
+	}, [localStorage.getItem("auth_token")])
+
+	const handleSignOut = () =>{
+		localStorage.removeItem("auth_token")
+		router.push("/auth/signin");
+	}
 	return (
 		<header className='nxt-my-6 nxt-w-98/100 nxt-mx-auto'>
 			<section className='nxt-flex nxt-justify-between nxt-items-center nxt-w-full'>
@@ -18,12 +31,25 @@ const Header = () => {
 						<li className='nxt-mx-2 nxt-text-xl'>Contacts</li>
 					</ul>
 					<ul className='nxt-flex'>
-						<li className='nxt-mx-2 nxt-text-xl'>
-							<Link href='/auth/signin'>Login</Link>
-						</li>
-						<li className='nxt-mx-2 nxt-text-xl'>
-							<Link href='/auth/signup'>Signup</Link>
-						</li>
+						{
+							
+							!token ?(
+								<>
+									<li className='nxt-mx-2 nxt-text-xl'>
+									<Link href='/auth/signin'>Login</Link>
+									</li>
+									<li className='nxt-mx-2 nxt-text-xl'>
+										<Link href='/auth/signup'>Signup</Link>
+									</li>
+								</>
+							):
+							(
+								<li className='nxt-mx-2 nxt-text-xl'>
+									<button onClick={handleSignOut}>Logout</button>
+								</li>
+							)
+							
+						}
 					</ul>
 				</div>
 			</section>
