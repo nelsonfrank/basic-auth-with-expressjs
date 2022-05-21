5f4dcc6b6b8dfa01365c126bea620b18bf850076
nelson frank <nelsonfr78@outlook.com>
2022-05-21 17:51:48 +0300

    feat: Add another domain url in cors origin option

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,11 @@ const app = express();
 // cors
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://note-app-wine.vercel.app"],
+    origin: [
+      "http://localhost:3000",
+      "https://note-app-wine.vercel.app",
+      "https://auth-expressjs.vercel.app",
+    ],
     credentials: true,
   })
 );
