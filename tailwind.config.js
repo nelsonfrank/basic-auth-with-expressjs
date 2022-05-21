7e09e342bb001f9dd7019ba6f1055fba9bc0e3c2
nelson frank <nelsonfr78@outlook.com>
2022-05-21 17:27:36 +0300

    fix: Update font to Inter

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
 module.exports = {
-	prefix: "nxt-",
-	purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-	darkMode: false, // or 'media' or 'class'
-	theme: {
-		fontFamily: {
-			quicksand: ["Quicksand", "sans-serif"],
-			sans: ["Graphik", "sans-serif"],
-			serif: ["Merriweather", "serif"],
-		},
-		extend: {
-			width: {
-				"98/100": "98%",
-			},
-		},
-	},
-	variants: {
-		extend: {},
-	},
-	plugins: [],
+  prefix: "nxt-",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      quicksand: ["Inter", "sans-serif"],
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      width: {
+        "98/100": "98%",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
