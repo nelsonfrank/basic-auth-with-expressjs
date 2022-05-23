756b74030a3e4bf322d4488e5bd96a3f70091f1b
nelson frank <nelsonfr78@outlook.com>
2022-05-23 13:33:58 +0300

    fix: Enable sameSite and secure cookie options

--- backend/src/modules/auth/auth.controller.js
+++ backend/src/modules/auth/auth.controller.js
@@ -54,13 +54,13 @@ exports.signInController = async (req, res) => {
         res
           .status(200)
           .cookie("auth_token", authToken, {
-            // sameSite: "none",
-            // secure: true,
+            sameSite: "none",
+            secure: true,
             httpOnly: true,
           })
           .cookie("refresh_token", refreshToken, {
-            // sameSite: "none",
-            // secure: true,
+            sameSite: "none",
+            secure: true,
             httpOnly: true,
           })
           .json({ ...other, status: "logged-in" });
